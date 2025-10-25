c19cc40e47040540fa811baaae82f2a6003edfc8
Nimesh Sankalpa <nimeshsankalpa7@gmail.com>
2025-10-25 11:17:59 +0530

    Change 'Sign in' button text to 'Log in'

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -89,7 +89,7 @@ export default function Layout({ children }: LayoutProps) {
                     href="/login"
                     className="px-4 py-2 text-sm font-medium text-gray-700 hover:text-gray-900 transition-colors"
                   >
-                    Sign in
+                    Log in
                   </Link>
                   <Link
                     href="/signup"
